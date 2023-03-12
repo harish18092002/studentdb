b0aee461ca9b0da06a19c4be12c96d3f1f9f61fe
Harish A <harish18092002@gmail.com>
2023-03-12 23:22:52 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  // component: AppComponent,
+  children: [
+    { path: 'home', component: MainComponent },
+    { path: 'details', component: DetailsComponent },
+    { path: '**', redirectTo: 'home' },
+  ],
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
